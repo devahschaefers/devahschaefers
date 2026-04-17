1ee95ac124a398952890c16106e9c731e61646bf
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2026-04-16 22:17:24 -0700

    add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "devahschaefers.github.io",
+  base: "personalwebsite",
 });
