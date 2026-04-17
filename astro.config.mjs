8938273dc6df2b75a931c1e5fe1c7bc480f7debd
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2026-04-16 23:59:26 -0700

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: "devahschaefers.github.io",
+  site: "https://devahschaefers.github.io",
   base: "personalwebsite",
 });
